8a49ee2f9d24611961a37fbc37299def5d48239f
Manuel Bua <manuel.bua@gmail.com>
2013-02-17 21:15:31 +0100

    [bump] Bumped version to v1.0.6 [updated] CHANGELOG and changelog auto-build script

--- postprocessing/mk-changelog.sh
+++ postprocessing/mk-changelog.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-if [ -z "$2" ]; then
-	# use provided exp
-	#git log postprocessing "$1" --date=short --format="%ad%x20%an <%ae> %n%n%x09* %s%n" | sed 's/@/[at]/'
-	git log --pretty --numstat --summary "$1" master . | git2cl | sed 's/\<\(.*\)\(@\)\(.*\)/\1[at]\3/'
-else
-	# compose a very basic exp, "between version tags"
-	#git log postprocessing "$1".."$2" --date=short --format="%ad%x20%an <%ae> %n%n%x09* %s%n" | sed 's/@/[at]/'
-	git log --pretty --numstat --summary "$1".."$2" master . | git2cl | sed 's/\<\(.*\)\(@\)\(.*\)/\1[at]\3/'
-fi
+git log --pretty  --summary HEAD master . | git2cl | sed 's/\*\s\:\s//' | sed 's/\<\(.*\)\(@\)\(.*\)/\1[at]\3/'
