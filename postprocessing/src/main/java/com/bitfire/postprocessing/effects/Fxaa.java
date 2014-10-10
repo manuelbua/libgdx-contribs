531d184469a80d8c6c86d1810e1b75e66eb82d3d
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2014-10-10 11:02:29 +0200

    FXAA files formatted with eclipse-formatter.xml

--- postprocessing/src/main/java/com/bitfire/postprocessing/effects/Fxaa.java
+++ postprocessing/src/main/java/com/bitfire/postprocessing/effects/Fxaa.java
@@ -20,52 +20,46 @@ import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.bitfire.postprocessing.PostProcessorEffect;
 import com.bitfire.postprocessing.filters.FxaaFilter;
 
-/**
- * Implements the fast approximate anti-aliasing. Very fast and useful for
- * combining with other post-processing effects.
- * @author Toni Sagrista
- *
- */
+/** Implements the fast approximate anti-aliasing. Very fast and useful for combining with other post-processing effects.
+ * @author Toni Sagrista */
 public final class Fxaa extends PostProcessorEffect {
-    private FxaaFilter fxaaFilter = null;
+	private FxaaFilter fxaaFilter = null;
 
-    /** Creatie a Fxaa with the viewport size */
-    public Fxaa(int viewportWidth, int viewportHeight) {
-	setup(viewportWidth, viewportHeight);
-    }
-
-    private void setup(int viewportWidth, int viewportHeight) {
-	fxaaFilter = new FxaaFilter(viewportWidth, viewportHeight);
-    }
-
-    public void setViewportSize(int width, int height) {
-	fxaaFilter.setViewportSize(width, height);
-    }
-
-    /**
-     * Sets the span max parameter. The default value is 8.
-     * @param value
-     */
-    public void setSpanMax(float value) {
-	fxaaFilter.setFxaaSpanMax(value);
-    }
-
-    @Override
-    public void dispose() {
-	if (fxaaFilter != null) {
-	    fxaaFilter.dispose();
-	    fxaaFilter = null;
+	/** Creatie a Fxaa with the viewport size */
+	public Fxaa (int viewportWidth, int viewportHeight) {
+		setup(viewportWidth, viewportHeight);
 	}
-    }
 
-    @Override
-    public void rebind() {
-	fxaaFilter.rebind();
-    }
+	private void setup (int viewportWidth, int viewportHeight) {
+		fxaaFilter = new FxaaFilter(viewportWidth, viewportHeight);
+	}
 
-    @Override
-    public void render(FrameBuffer src, FrameBuffer dest) {
-	restoreViewport(dest);
-	fxaaFilter.setInput(src).setOutput(dest).render();
-    }
+	public void setViewportSize (int width, int height) {
+		fxaaFilter.setViewportSize(width, height);
+	}
+
+	/** Sets the span max parameter. The default value is 8.
+	 * @param value */
+	public void setSpanMax (float value) {
+		fxaaFilter.setFxaaSpanMax(value);
+	}
+
+	@Override
+	public void dispose () {
+		if (fxaaFilter != null) {
+			fxaaFilter.dispose();
+			fxaaFilter = null;
+		}
+	}
+
+	@Override
+	public void rebind () {
+		fxaaFilter.rebind();
+	}
+
+	@Override
+	public void render (FrameBuffer src, FrameBuffer dest) {
+		restoreViewport(dest);
+		fxaaFilter.setInput(src).setOutput(dest).render();
+	}
 }
